9fa7a15d27960f68812c5a404b8d6ec895c3b9e9
unknown <Muhammadsarim203@gmail.com>
2022-02-02 23:18:10 +0500

    java start

---
+++ src/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+
+    public static void main(String[] args){
+
+//=============== VARIABLES
+        int a = 10;
+        int b = 5;
+        int ans = (a * b) / (a -b);
+
+        System.out.println(ans);
+
+
+
+    }
+
+}
---
+++ src/javaInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+import java.util.*;
+
+public class javaInput {
+
+    public static void main(String[] args){
+
+//      ====== input in java =====
+        Scanner sc = new Scanner(System.in);    // sc is an object assign to scanner
+//        String name = sc.next();                // next function will take only one token
+
+          String name = sc.nextLine();     // it will take all tokens
+          System.out.println(name);
+
+
+//       ===== other function of scanners =====//
+//        ===== nextInt() ======//
+//        ===== nextFloat() ======//
+
+    }
+}
