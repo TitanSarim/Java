2b26049181d0ecb06948e31eac3d05d2abcb65ad
unknown <Muhammadsarim203@gmail.com>
2022-02-04 19:29:36 +0500

    java loops

---
+++ src/dowhileloop.java
@@ -0,0 +1,14 @@
+public class dowhileloop {
+
+    public static void main(String args[]){
+
+            int i = 12;
+            do{
+                System.out.println("sarim");
+                i++;
+            }while (i < 1000000000);
+
+    }
+
+
+}
---
+++ src/forloop.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class forloop {
+
+    public static void main(String args[]){
+
+//            for(int i = 0; i < 10; i++){
+//                System.out.println(i + " ");
+//            }
+
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+//            int sum = 0;
+//            for (int i = 1; i<=n;  i++){
+//                sum = sum + i;
+//            }
+//            System.out.println(sum);
+
+            for (int i = 0; i<11; i++){
+                System.out.println(i*n);
+            }
+    }
+
+
+}
+
